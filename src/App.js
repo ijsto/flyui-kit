5b91ec397112606d601cc7a5a05004892aec992d
ScottAgirs <scott@ijs.to>
2021-09-21 15:51:01 +0300

    Initial KABOOOM 💥
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          JavaScript UI Kit
+        </p>
+        <a
+          className="App-link"
+          href="https://ijs.to/@scottagirs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          by @scottagirs
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
