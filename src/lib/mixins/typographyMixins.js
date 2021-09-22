e79e468e34b2e02dd2bfa50059a2c5dc0a21a74c
ScottAgirs <scott@ijs.to>
2021-09-22 17:05:00 +0300

    feat(Typography): Init mixin fontstack constants
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ src/lib/mixins/typographyMixins.js
@@ -0,0 +1,12 @@
+// Core Stacks
+export const monoFontStack = 'Courier New, sans-serif';
+export const sansSerifFontStack = 'Avant Garde, sans-serif';
+export const fancyFontStack = 'Brush Script MT, sans-serif';
+export const serifFontStack = 'Cambria, serif';
+export const boldSerifFontStack = 'Cambria, serif';
+
+// Global UI Stacks
+export const buttonFontStack = sansSerifFontStack;
+export const captionTextFontStack = sansSerifFontStack;
+export const headingFontStack = serifFontStack;
+export const inputFontStack = sansSerifFontStack;
