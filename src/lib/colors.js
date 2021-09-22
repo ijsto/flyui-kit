f1617c4e6e9b2d232e9dcf256ef861cb0b4439e7
ScottAgirs <scott@ijs.to>
2021-09-22 15:51:02 +0300

    refactor(Colors): Add base and text colors
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -3,69 +3,121 @@
 
 // Our colour palette system [WIP]
 
+// Base Palette colors
 const backgroundCol = '#F7F7F7';
-const borderCol = '#B838F0';
+const backgroundDarkCol = '#F7F7F7';
+const backgroundLightCol = '#F7F7F7';
 const errorCol = '#F21368';
+const errorDarkCol = '#F21368';
+const errorLightCol = '#F21368';
 const infoCol = '#38B8F0';
-const inactiveCol = '#A3A1A8';
-const linkTextCol = '#B838F0';
+const infoDarkCol = '#38B8F0';
+const infoLightCol = '#38B8F0';
 const primaryCol = '#B838F0';
+const primaryDarkCol = '#B838F0';
+const primaryLightCol = '#B838F0';
 const secondaryCol = '#F21368';
+const secondaryDarkCol = '#F21368';
+const secondaryLightCol = '#F21368';
+const successCol = 'green';
+const successDarkCol = 'green';
+const successLightCol = 'green';
 const surfaceCol = '#FFF';
-const svgFillCol = '#333644';
-const svgFillHoverCol = '#B838F0';
-const contrastSurfaceCol = '#333644';
-const contrastTextCol = '#bdd9f2';
+const surfaceDarkCol = '#FFF';
+const surfaceLightCol = '#FFF';
 const textCol = '#333644';
-const textPlaceholderCol = '#c7bfc9';
-const trueWhite = '#FFFFFF';
-const whiteCol = '#fff';
+const textDarkCol = '#333644';
+const textLightCol = '#333644';
+const textOnBackgroundCol = 'white';
+const textOnBackgroundDarkCol = 'white';
+const textOnBackgroundLightCol = 'white';
+const textOnErrorCol = 'white';
+const textOnErrorDarkCol = 'white';
+const textOnErrorLightCol = 'white';
+const textOnInfoCol = 'white';
+const textOnInfoDarkCol = 'white';
+const textOnInfoLightCol = 'white';
+const textOnPrimaryCol = 'white';
+const textOnPrimaryDarkCol = 'white';
+const textOnPrimaryLightCol = 'white';
+const textOnSecondaryCol = 'white';
+const textOnSecondaryDarkCol = 'white';
+const textOnSecondaryLightCol = 'white';
+const textOnSuccessCol = 'white';
+const textOnSuccessDarkCol = 'white';
+const textOnSuccessLightCol = 'white';
+const textOnSurfaceCol = 'white';
+const textOnSurfaceDarkCol = 'white';
+const textOnSurfaceLightCol = 'white';
+const textOnWarningCol = 'white';
+const textOnWarningDarkCol = 'white';
+const textOnWarningLightCol = 'white';
+const warningCol = '#F17B0D';
+const warningDarkCol = '#F17B0D';
+const warningLightCol = '#F17B0D';
+
+// // Derived colors
+// const borderCol = primaryCol;
+// const inactiveCol = '#A3A1A8';
+// const linkTextCol = primaryCol;
+// const svgFillCol = '#333644';
+// const svgFillHoverCol = '#B838F0';
+// const contrastSurfaceCol = '#333644';
+// const contrastTextCol = '#bdd9f2';
+// const textPlaceholderCol = '#c7bfc9';
+// const trueWhite = '#FFFFFF';
+// const whiteCol = '#fff';
 
 export const colors = {
   background: backgroundCol,
-  border: borderCol,
-  contrastSurface: contrastSurfaceCol,
-  contrastText: contrastTextCol,
+  backgroundDark: backgroundDarkCol,
+  backgroundLight: backgroundLightCol,
   error: errorCol,
-  fadedPrimary: '#457980',
-  // fadedPrimaryRGB: Color('#457980').rgb().array().join(','),
-  inactive: inactiveCol,
-  // inactiveRGB: Color(inactiveCol).rgb().array().join(','),
+  errorDark: errorDarkCol,
+  errorLight: errorLightCol,
   info: infoCol,
-  // infoRGB: Color(infoCol).rgb().array().join(','),
-  linkText: linkTextCol,
-  ltAccent: '#457980',
-  // ltAccentRGB: Color('#457980').rgb().array().join(','),
-  ltBlue: '#0086FF',
-  // ltBlueRGB: Color('#0086FF').rgb().array().join(','),
-  ltGrey: '#e2e2e2',
-  // ltGreyRGB: Color('#e2e2e2').rgb().array().join(','),
-  placeholder: textPlaceholderCol,
+  infoDark: infoDarkCol,
+  infoLight: infoLightCol,
   primary: primaryCol,
-  // primaryRGB: Color(primaryCol).rgb().array().join(','),
-  primaryShade: [
-    // Color(primaryCol).lighten(0.5).hex(),
-    // Color(primaryCol).lighten(0.25).hex(),
-    // Color(primaryCol).hex(),
-    // Color(primaryCol).darken(0.25).hex(),
-    // Color(primaryCol).darken(0.5).hex(),
-  ],
+  primaryDark: primaryDarkCol,
+  primaryLight: primaryLightCol,
   secondary: secondaryCol,
-  // secondaryRGB: Color(secondaryCol).rgb().array().join(','),
-  surface: surfaceCol,
-  // surfaceRGB: Color(surfaceCol).rgb().array().join(','),
-  surfaceShade: [
-    // Color(surfaceCol).lighten(0.5).hex(),
-    // Color(surfaceCol).lighten(0.25).hex(),
-    // Color(surfaceCol).hex(),
-    // Color(surfaceCol).darken(0.25).hex(),
-    // Color(surfaceCol).darken(0.5).hex(),
-  ],
-  svgFill: svgFillCol,
-  svgHoverFill: svgFillHoverCol,
+  secondaryDark: secondaryDarkCol,
+  secondaryLight: secondaryLightCol,
+  success: surfaceCol,
+  successDark: successDarkCol,
+  successLight: successLightCol,
+  surface: successCol,
+  surfaceDark: surfaceDarkCol,
+  surfaceLight: surfaceLightCol,
   text: textCol,
-  trueWhite,
-  // trueWhiteRGB: Color(trueWhite).rgb().array().join(','),
-  white: whiteCol,
-  // whiteRGB: Color(whiteCol).rgb().array().join(','),
+  textDark: textDarkCol,
+  textLight: textLightCol,
+  textOnBackground: textOnBackgroundCol,
+  textOnBackgroundDark: textOnBackgroundDarkCol,
+  textOnBackgroundLight: textOnBackgroundLightCol,
+  textOnError: textOnErrorCol,
+  textOnErrorDark: textOnErrorDarkCol,
+  textOnErrorLight: textOnErrorLightCol,
+  textOnInfo: textOnInfoCol,
+  textOnInfoDark: textOnInfoDarkCol,
+  textOnInfoLight: textOnInfoLightCol,
+  textOnPrimary: textOnPrimaryCol,
+  textOnPrimaryDark: textOnPrimaryDarkCol,
+  textOnPrimaryLight: textOnPrimaryLightCol,
+  textOnSecondary: textOnSecondaryCol,
+  textOnSecondaryDark: textOnSecondaryDarkCol,
+  textOnSecondaryLight: textOnSecondaryLightCol,
+  textOnSuccess: textOnSuccessCol,
+  textOnSuccessDark: textOnSuccessDarkCol,
+  textOnSuccessLight: textOnSuccessLightCol,
+  textOnSurface: textOnSurfaceCol,
+  textOnSurfaceDark: textOnSurfaceDarkCol,
+  textOnSurfaceLight: textOnSurfaceLightCol,
+  textOnWarning: textOnWarningCol,
+  textOnWarningDark: textOnWarningDarkCol,
+  textOnWarningLight: textOnWarningLightCol,
+  warning: warningCol,
+  warningDark: warningDarkCol,
+  warningLight: warningLightCol,
 };
