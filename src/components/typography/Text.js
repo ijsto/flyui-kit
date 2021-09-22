7c252a4145a5c62511e74db17339451d7da00076
ScottAgirs <scott@ijs.to>
2021-09-22 15:51:58 +0300

    feat(Typography): Init Text />
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ src/components/typography/Text.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { color, space, typography, layout } from 'styled-system';
+
+import Box from '../layout/Box';
+
+const StyledText = styled(Box)`
+  ${color};
+  ${layout};
+  ${space};
+  ${typography};
+  ${({ clickable }) =>
+    clickable &&
+    css`
+      cursor: pointer;
+    `}
+  ${({ transform }) =>
+    css`
+      text-transform: ${transform};
+    `}
+`;
+
+const Text = ({ children, my = 0, onClick, ...rest }) => (
+  <StyledText
+    my={my}
+    tabIndex={onClick || rest.variant === 'link' ? 0 : null}
+    // @TODO: Add keydown and a11y handlers
+    // onKeyDown={e => a11yClick(e, onClick)}
+    onClick={onClick}
+    clickable={Boolean(onClick)}
+    {...rest}
+  >
+    {children}
+  </StyledText>
+);
+
+export default Text;
