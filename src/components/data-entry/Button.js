6165accd9efaa13049d4dde4038408f2aff1b3c3
ScottAgirs <scott@ijs.to>
2021-09-22 15:52:53 +0300

    feat(Data Entry): Init Button /> V1
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/components/data-entry/Button.js
+++ src/components/data-entry/Button.js
@@ -1,7 +1,195 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+import { space, layout } from 'styled-system';
 
-const Button = ({children, ...rest}) => {
-  return(<button style={{ padding: "4px 16px", background:"beige"}} {...rest}>{children}</button>);
-}
- 
+import SVG from '../data-display/SVG';
+import LoadingIcon from '../../icons/circle-notch.svg';
+import Box from '../layout/Box';
+import Text from '../typography/Text';
+
+import {
+  buttonShapeStyles,
+  buttonSizeStyles,
+  buttonVariantStyles,
+} from '../../lib/mixins/buttonMixins';
+
+export const ButtonHeadingTag = styled.button`
+  appearance: none;
+  background: transparent;
+  border: 0;
+  display: block;
+  outline: none;
+  ${({ active }) => active && `color: ${({ theme }) => theme.colors.primary};`}
+  // {transition}
+  cursor: pointer;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    text-decoration: underline;
+    margin: 0.5em;
+  }
+
+  &:focus,
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+    // {transition};
+  }
+`;
+
+export const StyledButtonLink = styled.a`
+  ${buttonSizeStyles};
+  ${buttonShapeStyles};
+`;
+
+const StyledButton = styled.button`
+  ${({ inline }) => (inline ? 'display: inline;' : 'display: block;')};
+  ${buttonSizeStyles};
+  ${buttonShapeStyles};
+
+  ${({ unstyled }) => !unstyled && buttonVariantStyles};
+`;
+
+export const StyledButtonWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  span {
+    white-space: nowrap;
+  }
+`;
+
+const StyledButtonContent = styled.span`
+  ${layout};
+  ${space};
+`;
+export const StyledButtonIcon = styled.span`
+  ${space};
+  display: flex;
+  align-items: center;
+`;
+
+export const ButtonIcon = ({ icon, fill, size, variant, ...rest }) =>
+  icon ? (
+    <StyledButtonIcon {...rest}>
+      <SVG
+        fill={fill}
+        icon={icon}
+        noHover
+        hoverable
+        resetFill
+        size={size}
+        variant={variant}
+      />
+    </StyledButtonIcon>
+  ) : null;
+
+ButtonIcon.defaultProps = {
+  size: '16px',
+  variant: null,
+};
+
+const ButtonChildrenContainer = ({
+  children,
+  endIcon,
+  iconVariant,
+  iconsSize,
+  labelDisplay,
+  loading,
+  loadingLabel,
+  startIcon,
+}) => (
+  <StyledButtonWrapper>
+    {!loading && (
+      <ButtonIcon
+        variant={iconVariant}
+        icon={startIcon}
+        size={iconsSize}
+        // We have `mr` here to improve layout appearance when there's only icon and no label.
+        mr={children && 1}
+      />
+    )}
+
+    {loading ? (
+      // We have `mr` here to improve layout appearance when there's only icon and no label.
+      <Box display="flex" alignItems="center" mr={children && 1}>
+        <ButtonIcon icon={<LoadingIcon className="spin" />} size={16} />
+        {loadingLabel && <Text pl={2}>{loadingLabel}</Text>}
+      </Box>
+    ) : (
+      children && (
+        <StyledButtonContent display={labelDisplay} mx={1}>
+          {children}
+        </StyledButtonContent>
+      )
+    )}
+
+    {!loading && <ButtonIcon icon={endIcon} size={iconsSize} />}
+  </StyledButtonWrapper>
+);
+
+/**
+ * @param {React Component} SVGIcon Imported SVG React Component from path (example "@/icons/paper-plane.svg")
+ */
+export const ButtonLink = forwardRef(
+  (
+    {
+      children,
+      endIcon,
+      iconsSize,
+      fill,
+      labelDisplay,
+      loading,
+      loadingLabel,
+      startIcon,
+      target,
+      ...rest
+    },
+    ref
+  ) => (
+    <StyledButtonLink tabIndex="0" target={target} ref={ref} {...rest}>
+      <ButtonChildrenContainer
+        endIcon={endIcon}
+        iconsSize={iconsSize}
+        fill={fill}
+        labelDisplay={labelDisplay}
+        loading={loading}
+        loadingLabel={loadingLabel}
+        startIcon={startIcon}
+      >
+        {children}
+      </ButtonChildrenContainer>
+    </StyledButtonLink>
+  )
+);
+
+const Button = ({
+  children,
+  endIcon,
+  iconsSize,
+  iconVariant,
+  // Control label display ("block", "none"), useful, for example, if want to hide on mobile.
+  labelDisplay,
+  loading,
+  loadingLabel,
+  startIcon,
+  ...rest
+}) => (
+  <StyledButton type={rest.type || 'submit'} {...rest}>
+    <ButtonChildrenContainer
+      endIcon={endIcon}
+      iconsSize={iconsSize}
+      iconVariant={iconVariant}
+      labelDisplay={labelDisplay}
+      loading={loading}
+      loadingLabel={loadingLabel}
+      startIcon={startIcon}
+    >
+      {children}
+    </ButtonChildrenContainer>
+  </StyledButton>
+);
+
 export default Button;
