937976d95bdebb3c0eb63a298a83a2ec76260288
ScottAgirs <scott@ijs.to>
2021-09-23 23:04:45 +0300

    feat(SB): Add Text `inline` /> demo
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/components/data-display/Text.stories.js
+++ src/kit/components/data-display/Text.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FlyThemeProvider from '../FlyThemeProvider';
+import Box from '../layout/Box';
 import Text from './Text';
 
 export default {
@@ -26,19 +27,69 @@ Default.args = {
   children: 'Hello World!',
 };
 
-export const Themed = Template.bind({});
+export const Themed = args => (
+  <FlyThemeProvider theme={args.theme}>
+    <>{args.children}</>
+  </FlyThemeProvider>
+);
 Themed.argTypes = {
+  gradient: { control: { disable: true } },
   theme: {
     control: { type: 'object' },
     defaultValue: { fonts: { body: 'Times New Roman' } },
   },
+  variant: { control: { disable: true } },
 };
-Themed.decorators = [
+
+Themed.args = {
+  children: (
+    <>
+      <Text>In your app you can use</Text>
+
+      <Text gradient>
+        <code>{`<FlyThemeProvider theme={yourThemeObject} />`}</code>
+      </Text>
+
+      <Text>to override any of the default values.</Text>
+
+      <Text pt={3}>
+        Try replacing "Times New Roman" with "Arial", "Monaco" or any other
+        system font in the{' '}
+        <Text inline gradient>
+          <code>{`theme {}`}</code>
+        </Text>
+      </Text>
+    </>
+  ),
+};
+
+export const Inline = Template.bind({});
+Inline.argTypes = {
+  children: { control: { disable: true } },
+  gradient: { control: { disable: true } },
+  inline: {
+    control: { type: 'boolean' },
+    defaultValue: true,
+  },
+  variant: { control: { disable: true } },
+};
+
+Inline.decorators = [
   story => {
-    const { theme } = story();
-    return <FlyThemeProvider theme={theme}>{story()}</FlyThemeProvider>;
+    const { props } = story();
+    return (
+      <div>
+        <Box mb={3}>
+          Milord, behold -
+          <Text {...props} gradient border={1} mx={props.inline && 1}>
+            {story()}
+          </Text>
+          - toggle <code>inline</code> to see a grand difference.
+        </Box>
+      </div>
+    );
   },
 ];
-Themed.args = {
-  children: "I'm a Themed text!",
+Inline.args = {
+  children: <> this gradient text is indeed a {`<Text />`} </>,
 };
