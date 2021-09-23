d153affb6754ea53c30129afed09d32a6c4a9e9a
ScottAgirs <scott@ijs.to>
2021-09-23 12:46:04 +0300

    feat(exports): Add Theme, box, svg, text
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/index.js
+++ src/kit/index.js
@@ -1,3 +1,7 @@
+import Box from './components/layout/Box';
 import Button from './components/data-entry/Button';
+import Text from './components/data-display/Text';
+import FlyThemeProvider from './components/FlyThemeProvider';
+import SVG from './components/data-display/SVG';
 
-export { Button };
+export { FlyThemeProvider, Box, Button, SVG, Text };
