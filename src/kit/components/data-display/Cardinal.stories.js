04d0a056eaac9b4c13e761e0bf009287ca3c9132
ScottAgirs <scott@ijs.to>
2021-09-23 23:48:07 +0300

    feat(SB): Add Cardinal
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ src/kit/components/data-display/Cardinal.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Cardinal from './Cardinal';
+
+export default {
+  argTypes: {
+    align: {
+      control: { type: 'radio' },
+      defaultValue: true,
+      options: ['left', 'center', 'right'],
+    },
+    size: {
+      control: { type: 'select' },
+      defaultValue: true,
+      options: ['sm', 'md', 'lg'],
+    },
+  },
+  component: Cardinal,
+  title: 'Data Entry/Cardinal',
+};
+
+const Template = args => <Cardinal {...args}>{args.children}</Cardinal>;
+
+export const Default = Template.bind({});
+Default.args = {
+  align: 'left',
+  body: 'Cheer or birdie, birdie',
+  head: 'Cardinal',
+  size: 'md',
+};
