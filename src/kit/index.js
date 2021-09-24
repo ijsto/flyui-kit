1d2f6de77e7379a7fff72f430302279f3ca26f54
ScottAgirs <scott@ijs.to>
2021-09-24 15:50:31 +0300

    fix(Hot): Export Badge />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/index.js
+++ src/kit/index.js
@@ -2,6 +2,7 @@ import FlyGlobalStyle from './components/FlyGlobalStyle';
 import FlyThemeProvider from './components/FlyThemeProvider';
 
 import Avatar from './components/data-display/Avatar';
+import Badge from './components/data-display/Badge';
 import SVG from './components/data-display/SVG';
 import Text from './components/data-display/Text';
 import Tag from './components/data-display/Tag';
@@ -12,6 +13,7 @@ import Box from './components/layout/Box';
 
 export {
   Avatar,
+  Badge,
   Box,
   FlyGlobalStyle,
   FlyThemeProvider,
