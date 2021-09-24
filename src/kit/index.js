a40712c12a130ea11c29c3494fd9f14404dac19d
ScottAgirs <scott@ijs.to>
2021-09-24 15:10:38 +0300

    feat(Data Display): Init Tag />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/index.js
+++ src/kit/index.js
@@ -1,11 +1,22 @@
+import FlyGlobalStyle from './components/FlyGlobalStyle';
+import FlyThemeProvider from './components/FlyThemeProvider';
+
 import Avatar from './components/data-display/Avatar';
 import SVG from './components/data-display/SVG';
 import Text from './components/data-display/Text';
+import Tag from './components/data-display/Tag';
 
 import Button from './components/data-entry/Button';
 
 import Box from './components/layout/Box';
 
-import FlyThemeProvider from './components/FlyThemeProvider';
-
-export { Avatar, FlyThemeProvider, Box, Button, SVG, Text };
+export {
+  Avatar,
+  Box,
+  FlyGlobalStyle,
+  FlyThemeProvider,
+  Button,
+  SVG,
+  Tag,
+  Text,
+};
