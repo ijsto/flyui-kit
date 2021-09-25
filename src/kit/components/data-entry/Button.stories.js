02b8b8f73437e24935c2a8035f1da5902c1c5496
ScottAgirs <scott@ijs.to>
2021-09-25 17:24:14 +0300

    feat(SB): Add Button prop descriptions
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/components/data-entry/Button.stories.js
+++ src/kit/components/data-entry/Button.stories.js
@@ -7,18 +7,31 @@ export default {
   argTypes: {
     endIcon: {
       control: { type: 'select' },
+      description: 'Adds an icon to end',
       options: ['edit', 'cancel', 'save'],
     },
     hoverAnimation: {
       control: { type: 'select' },
       options: ['lift-low', 'lift', 'lift-high'],
     },
+    labelDisplay: {
+      control: { type: 'object' },
+      defaultValue: 'block',
+      description:
+        'This can be either a string (`block`, `none`, `inline` etc) or an array that will apply the display value to the label based on the breakpoint. This is useful to hide the button label on certain screen sizes. Example: `["none", "block"]` will hide the label on mobile.',
+    },
+    loading: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+      description:
+        'When `true`, replaces the label and the icons with the loading icon and `loadingLabel`, if provided.',
+    },
     shape: {
-      control: { type: 'radio' },
+      control: { type: 'inline-radio' },
       options: ['squared', 'regular', 'rounded', 'round'],
     },
     size: {
-      control: { type: 'radio' },
+      control: { type: 'inline-radio' },
       options: ['sm', 'md', 'lg'],
     },
     startIcon: {
@@ -26,7 +39,7 @@ export default {
       options: ['edit', 'cancel', 'save'],
     },
     typography: {
-      control: { type: 'radio' },
+      control: { type: 'inline-inline-radio' },
       options: ['serif', 'sans-serif', 'mono'],
     },
     unstyled: {
@@ -34,7 +47,7 @@ export default {
       defaultValue: false,
     },
     variant: {
-      control: { type: 'radio' },
+      control: { type: 'inline-radio' },
       options: ['primary', 'secondary', 'outline'],
     },
   },
