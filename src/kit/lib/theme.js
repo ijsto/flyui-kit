0de8d0267b54f7d89beb74c22a2b374f746b8619
ScottAgirs <scott@ijs.to>
2021-09-26 17:43:42 +0300

    refactor(Theme): Restructure + split & export parts
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/lib/theme.js
+++ src/kit/lib/theme.js
@@ -13,10 +13,7 @@ import {
   bodyFontStack,
 } from './constants';
 
-// @TODO: Refactor Theme:
-
-// - [-] borderProperties
-const borderProperties = {
+export const borderProps = {
   borders: [
     0,
     `1px solid ${colors.border}`,
@@ -27,17 +24,8 @@ const borderProperties = {
     `32px solid ${colors.border}`,
   ],
 };
-// - [ ] colorProperties
-// - [ ] shadowProperties
-// - [ ] spacingProperties
-// - [ ] typographyProperties
 
-// Properties
-
-const commonProperties = {
-  // @TODO: factor-out all the constants from theme
-  breakpoints: breakpointsPxArray,
-
+export const fontProps = {
   fontSizes: [
     '0.6rem',
     '0.7rem',
@@ -67,25 +55,9 @@ const commonProperties = {
     heading: headingFontStack,
     input: inputFontStack,
   },
-  heights: [16, 32, 64, 128, 256],
-  inputRadius: '0.25em',
-  inputRadiusLG: '5em',
-  inputRadiusSM: '0.125em',
+};
 
-  letterSpacings: {
-    mega: '0.25em',
-    normal: 'normal',
-    tight: '-0.05em',
-    tracked: '0.1em',
-  },
-  lineHeights: {
-    copy: 1.5,
-    solid: 1,
-    title: 1.25,
-  },
-
-  maxWidths: [16, 32, 64, 128, 256, 468, 512, 600, 768, 960, 1024, 1224, 1536],
-  radii: [0, 2, 4, 8, 16, 32, 64, 9999],
+export const effectProps = {
   shadows: {
     large: `0 15px 25px -4px var(--color-shadow-shadowColorRGB, rgba(0,0,0,0.25)),
         inset 0 -3px 4px -1px var(--color-shadow-shadowColorRGB, rgba(255,255,255,0.125)),
@@ -102,7 +74,6 @@ const commonProperties = {
         inset 0 0 5px 1px var(--color-shadow-innerShineColorRGB, rgba(255,255,255,0.125)),
         inset 0 20px 30px 0 var(--color-shadow-innerShineColorRGB, rgba(255,255,255,0.125))`,
   },
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   transition: {
     animation: 'cubic-bezier(0.28, 0.47, 0.83, 1.01)',
     speed: {
@@ -111,57 +82,76 @@ const commonProperties = {
       slow: '0.5s',
     },
   },
+};
+
+export const spaceProps = {
+  breakpoints: breakpointsPxArray,
+  heights: [16, 32, 64, 128, 256],
+  letterSpacings: {
+    mega: '0.25em',
+    normal: 'normal',
+    tight: '-0.05em',
+    tracked: '0.1em',
+  },
+  lineHeights: {
+    copy: 1.5,
+    solid: 1,
+    title: 1.25,
+  },
+  maxWidths: [16, 32, 64, 128, 256, 468, 512, 600, 768, 960, 1024, 1224, 1536],
+  radii: [0, 2, 4, 8, 16, 32, 64, 9999],
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   width: [16, 32, 64, 128, 256],
 };
 
 // eslint-disable-next-line prefer-destructuring
-commonProperties.radii.max = commonProperties.radii[7];
+fontProps.fontSizes.body = fontProps.fontSizes[4];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.body = commonProperties.fontSizes[4];
+fontProps.fontSizes.h1 = fontProps.fontSizes[9];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.h1 = commonProperties.fontSizes[9];
+fontProps.fontSizes.h2 = fontProps.fontSizes[8];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.h2 = commonProperties.fontSizes[8];
+fontProps.fontSizes.h3 = fontProps.fontSizes[7];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.h3 = commonProperties.fontSizes[7];
+fontProps.fontSizes.h4 = fontProps.fontSizes[5];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.h4 = commonProperties.fontSizes[5];
+fontProps.fontSizes.h5 = fontProps.fontSizes[4];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.h5 = commonProperties.fontSizes[4];
+fontProps.fontSizes.h6 = fontProps.fontSizes[3];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.h6 = commonProperties.fontSizes[3];
+fontProps.fontSizes.caption = fontProps.fontSizes[2];
 // eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.caption = commonProperties.fontSizes[2];
-// eslint-disable-next-line prefer-destructuring
-commonProperties.fontSizes.eyebrow = commonProperties.fontSizes[3];
+fontProps.fontSizes.eyebrow = fontProps.fontSizes[3];
 
-const radiusProperties = {
+// eslint-disable-next-line prefer-destructuring
+spaceProps.radii.max = spaceProps.radii[7];
+
+export const radiusProps = {
   radius: {
-    circle: commonProperties.radii.max,
-    lg: commonProperties.radii[4],
-    max: commonProperties.radii.max,
-    md: commonProperties.radii[3],
-    sm: commonProperties.radii[1],
-    square: commonProperties.radii[0],
+    circle: spaceProps.radii.max,
+    lg: spaceProps.radii[4],
+    max: spaceProps.radii.max,
+    md: spaceProps.radii[3],
+    sm: spaceProps.radii[1],
+    square: spaceProps.radii[0],
     // eslint-disable-next-line sort-keys
     input: {
-      regular: commonProperties.radii[2],
-      round: commonProperties.radii.max,
-      rounded: commonProperties.radii[3],
-      square: commonProperties.radii[0],
-      squared: commonProperties.radii[1],
+      regular: spaceProps.radii[2],
+      round: spaceProps.radii.max,
+      rounded: spaceProps.radii[3],
+      square: spaceProps.radii[0],
+      squared: spaceProps.radii[1],
     },
   },
 };
 
-const theme = {
-  ...commonProperties,
-  ...borderProperties,
-  ...radiusProperties,
+export const theme = {
+  ...borderProps,
+  ...effectProps,
+  ...fontProps,
+  ...radiusProps,
+  ...spaceProps,
   colors,
-  skeletonColor: colors.fadedPrimary,
-  skeletonShineColor: colors.secondary,
-  // eslint-disable-next-line sort-keys
   name: 'light',
 };
 
