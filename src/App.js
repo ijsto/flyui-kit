33df737fdf53ccfe8170fbdb8f9e63dbc97c5882
ScottAgirs <scott@ijs.to>
2021-09-28 00:04:04 +0300

    refactor: Add React playground
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          JavaScript UI Kit
-        </p>
+        <img src="logo" className="App-logo" alt="logo" />
+        <p>JavaScript UI Kit</p>
         <a
           className="App-link"
           href="https://ijs.to/@scottagirs"
