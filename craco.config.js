17faaf8475d676f0b5d969584ede7b9cb94751bd
ScottAgirs <scott@ijs.to>
2021-09-27 21:23:50 +0300

    chore(CRACO): cleanup
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- craco.config.js
+++ craco.config.js
@@ -3,11 +3,7 @@ const path = require(`path`);
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src/kit'),
-      '@components': path.resolve(__dirname, 'src/kit/components'),
-      '@icons': path.resolve(__dirname, 'src/kit/icons'),
-      '@lib': path.resolve(__dirname, 'src/kit/lib'),
-      '@utils': path.resolve(__dirname, 'src/kit/utils'),
+      '@/*': path.resolve(__dirname, 'src/kit/*'),
     },
   },
 };
