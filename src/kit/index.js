879fda4fcef23ddfc5434a651d77bcda22f1479f
ScottAgirs <scott@ijs.to>
2021-09-29 16:45:24 +0300

    feat(Fly): Export `Stepper`, `Step`, `Dot />`
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/index.js
+++ src/kit/index.js
@@ -8,6 +8,7 @@ import SVG from './components/data-display/SVG';
 import Text from './components/data-display/Text';
 import Tag from './components/data-display/Tag';
 
+import Dot from './components/feedback/Dot';
 import Modal from './components/feedback/Modal';
 import Popover from './components/feedback/Popover';
 
@@ -18,6 +19,8 @@ import Input from './components/data-entry/Input';
 import Box from './components/layout/Box';
 import Stack from './components/layout/Stack';
 
+import Stepper, { Step } from './components/navigation/Stepper';
+
 export {
   Avatar,
   Badge,
@@ -25,6 +28,7 @@ export {
   Button,
   ButtonLink,
   Cardinal,
+  Dot,
   FlyGlobalStyle,
   FlyThemeProvider,
   Input,
@@ -32,6 +36,8 @@ export {
   Popover,
   SVG,
   Stack,
+  Stepper,
+  Step,
   Tag,
   Text,
 };
