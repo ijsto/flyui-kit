07588ff6d68cfdea4efee903ab61ebf6d64e7bd3
ScottAgirs <scott@ijs.to>
2021-12-05 15:26:41 +0200

    refactor(Data Display): Cardinal />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/components/data-display/Cardinal.js
+++ src/kit/components/data-display/Cardinal.js
@@ -5,6 +5,17 @@ import Text from './Text';
 import Box from '../layout/Box';
 
 const StyledCardinal = styled(Box)`
+  ${({ orientation, theme, spaceX }) =>
+    orientation === 'horizontal'
+      ? css`
+          align-items: center;
+          display: flex;
+          grid-gap: ${theme.space[spaceX]}px;
+        `
+      : css`
+          display: inline-block;
+        `}
+
   ${({ transform }) =>
     css`
       text-transform: ${transform};
@@ -12,23 +23,74 @@ const StyledCardinal = styled(Box)`
 `;
 
 const Cardinal = ({
-  align = 'left',
+  align,
   children,
   body,
-  bodyVariant = 'caption',
+  bodyAs,
+  bodyFontSize,
+  bodyFontWeight,
   head,
-  headVariant = 'eyebrow',
-}) => (
-  <StyledCardinal display="inline-block" textAlign={align}>
-    <Text mb={0} pb={1} variant={headVariant}>
-      {head}
-    </Text>
+  headAs,
+  headFontSize,
+  headFontWeight,
+  items: cardinals,
+  orientation,
+  spaceX,
+}) => {
+  const cardinalBaseProps = {
+    orientation,
+    spaceX,
+    textAlign: align,
+  };
+  console.log('cardinalBaseProps', cardinalBaseProps);
+  const headBaseProps = {
+    as: headAs,
+    fontSize: headFontSize,
+    fontWeight: headFontWeight,
+    m: 0,
+    p: 0,
+    pb: 1,
+  };
+  const bodyBaseProps = {
+    as: bodyAs,
+    fontSize: bodyFontSize,
+    fontWeight: bodyFontWeight,
+    m: 0,
+    p: 0,
+  };
 
-    <Text mb={0} pb={1} variant={bodyVariant}>
-      {body}
-    </Text>
-    {children}
-  </StyledCardinal>
-);
+  return (
+    <Box as="dl" display="flex" flexWrap="wrap" m={0} p={0}>
+      <StyledCardinal {...cardinalBaseProps}>
+        <Text {...headBaseProps}>{head}</Text>
 
+        <Text {...bodyBaseProps}>{body}</Text>
+        {children}
+      </StyledCardinal>
+
+      {cardinals?.length > 0 &&
+        cardinals.map((c, i) => (
+          <StyledCardinal key={`fly-cardinal-${i}`} {...cardinalBaseProps}>
+            <Text {...headBaseProps}>{c.head}</Text>
+
+            <Text {...bodyBaseProps}>{c.body}</Text>
+            {c.children}
+          </StyledCardinal>
+        ))}
+    </Box>
+  );
+};
+
+Cardinal.defaultProps = {
+  align: 'left',
+  bodyAs: 'dd',
+  bodyFontSize: 'sm',
+  bodyFontWeight: 'light',
+  headAs: 'dt',
+  headFontSize: 'md',
+  headFontWeight: 'bold',
+  items: [],
+  orientation: 'vertical',
+  spaceX: 2,
+};
 export default Cardinal;
