5f7941276083a334b65115b46798e520a44c69ca
ScottAgirs <scott@ijs.to>
2021-12-05 15:28:57 +0200

    refactor(layout): add onClick to Box />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/components/layout/Box.js
+++ src/kit/components/layout/Box.js
@@ -1,5 +1,5 @@
 import React, { forwardRef } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import {
   border,
   borderRadius,
@@ -12,6 +12,8 @@ import {
   typography,
 } from 'styled-system';
 
+import { a11yClick } from '../../utils/a11y';
+
 const StyledBox = styled.div`
   ${border};
   ${borderRadius};
@@ -22,12 +24,36 @@ const StyledBox = styled.div`
   ${layout};
   ${position};
   ${typography};
+
+  ${({ onClick }) =>
+    onClick &&
+    css`
+      cursor: pointer;
+    `}
 `;
 
-const Box = forwardRef(({ children, ...rest }, ref) => (
-  <StyledBox ref={ref} {...rest}>
-    {children}
-  </StyledBox>
-));
+const Box = forwardRef(({ children, gap, gridGap, ...rest }, ref) => {
+  const isButtonLike = Boolean(rest.onClick);
+
+  const buttonRoleProps = isButtonLike
+    ? {
+        'aria-disabled': rest.disabled,
+        onKeyDown: e => a11yClick(e, rest.onClick),
+        role: 'button',
+        tabIndex: 0,
+      }
+    : {};
+
+  return (
+    <StyledBox
+      ref={ref}
+      gridGap={gridGap || gap}
+      {...buttonRoleProps}
+      {...rest}
+    >
+      {children}
+    </StyledBox>
+  );
+});
 
 export default Box;
