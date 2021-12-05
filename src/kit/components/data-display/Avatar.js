8e9055a1f384dc24d1aa9bd95e711caf04e380be
ScottAgirs <scott@ijs.to>
2021-12-05 15:26:24 +0200

    refactor(Data Display): Avatar />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/kit/components/data-display/Avatar.js
+++ src/kit/components/data-display/Avatar.js
@@ -3,65 +3,45 @@ import styled, { css } from 'styled-components';
 
 import Box from '../layout/Box';
 
-const switchShapeRadius = shape => {
-  switch (shape) {
-    case 'round':
-      return 'max';
-    case 'rounded':
-      return 4;
-    case 'regular':
-      return 3;
-    case 'squared':
-      return 2;
-    case 'square':
-      return 0;
+import { getRadius, getSize } from '../../utils/sizing';
 
-    default:
-      return 0;
-  }
-};
+const sizingStyles = ({ height, size, width, theme }) => css`
+  height: ${height || getSize({ size, theme })};
+  min-height: ${height || getSize({ size, theme })};
+  width: ${width || getSize({ size, theme })};
+  min-width: ${width || getSize({ size, theme })};
+`;
 
-const avatarBaseStyles = css`
-  align-items: center;
-  justify-content: center;
-  display: inline-flex;
-  height: ${({ size, height }) => height || size};
-  min-height: ${({ size, height }) => height || size};
-  width: ${({ size, width }) => width || size};
-  min-width: ${({ size, width }) => width || size};
+const StyledAvatar = styled.div`
+  background-color: ${({ bg, theme }) => bg || theme.colors.background};
   overflow: hidden;
   position: relative;
-`;
-
-const StyledAvatar = styled(Box)`
-  aspect-ratio: 1/1;
-  background-color: ${({ theme }) => theme.colors.background};
-  background-image: url('${({ src }) => src}');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  ${avatarBaseStyles};
-
   img {
-    border-radius: ${({ shape, theme }) =>
-      theme.radii[switchShapeRadius(shape)]}px;
-    height: auto;
-    max-height: 100%;
-    width: auto;
     max-width: 100%;
   }
+  ${({ src }) =>
+    !src &&
+    css`
+      align-items: center;
+      border-radius: ${({ radius, theme }) => getRadius({ radius, theme })};
+      color: ${({ theme }) => theme?.colors?.text || 'inherit'};
+      display: inline-flex;
+      justify-content: center;
+      ${sizingStyles}
+    `};
+
+  ${({ src }) =>
+    src &&
+    css`
+      img {
+        ${sizingStyles}
+        border-radius: ${({ radius, theme }) => getRadius({ radius, theme })};
+        max-width: 100%;
+      }
+    `}
 `;
 
-const StyledContentAvatar = styled(Box)`
-  align-items: center;
-  display: flex;
-  font-size: 75%;
-  justify-content: center;
-  background-color: ${({ theme }) => theme.colors.primary};
-  ${avatarBaseStyles};
-`;
-
-const defaultSizes = ['ty', 'sm', 'md', 'lg', 'xl'];
+const defaultAvatarSizes = ['ty', 'sm', 'md', 'lg', 'xl'];
 const normalizeSize = size => {
   switch (size) {
     case 'ty':
@@ -80,44 +60,40 @@ const normalizeSize = size => {
   }
 };
 
-const Avatar = ({ children, outline, outlineColor, outlineWidth, ...rest }) => {
-  const normalizedSize = defaultSizes.includes(rest.size)
+const Avatar = ({
+  alt,
+  borderRadius,
+  Component,
+  componentProps,
+  initials,
+  radius,
+  src,
+  ...rest
+}) => {
+  const normalizedSize = defaultAvatarSizes.includes(rest.size)
     ? normalizeSize(rest.size)
     : rest.size;
 
-  if (rest.shape) {
-    // @TODO: Calculate borderRadius based on size
-    // eslint-disable-next-line no-param-reassign
-    rest.borderRadius = switchShapeRadius(rest.shape);
-  }
+  const ImageComponent = Component || 'img';
+  const foreignComponentProps = Component ? { componentProps } : {};
 
-  if (outline) {
-    // eslint-disable-next-line no-param-reassign
-    rest.border = 1;
-    // eslint-disable-next-line no-param-reassign
-    rest.borderColor = outlineColor || undefined;
-    // eslint-disable-next-line no-param-reassign
-    rest.borderWidth = outlineWidth || undefined;
-  }
-
-  if (children) {
-    return (
-      <StyledContentAvatar {...rest} size={normalizedSize}>
-        {children}
-      </StyledContentAvatar>
-    );
-  }
+  const avatarContainerProps = {
+    as: 'span',
+    radius,
+    size: normalizedSize,
+    ...rest,
+  };
 
   return (
-    <StyledAvatar {...rest} size={normalizedSize}>
-      <img src={rest.src} alt="" />
+    <StyledAvatar {...avatarContainerProps}>
+      {src && <ImageComponent src={src} alt={alt} {...foreignComponentProps} />}
+      {!src && initials && <Box role="img">{initials}</Box>}
     </StyledAvatar>
   );
 };
 
 Avatar.defaultProps = {
   radius: '50%',
-  shape: 'round',
   size: '28px',
   src: '',
 };
